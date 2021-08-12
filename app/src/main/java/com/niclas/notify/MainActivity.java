fd742ecd7491d935c7b0c67f4715e0b28634629f
Carpenteri1 <niclastimle@protonmail.com>
2021-08-12 14:43:13 +0200

    notifyes are now stored when app shutdown

--- app/src/main/java/com/niclas/notify/MainActivity.java
+++ app/src/main/java/com/niclas/notify/MainActivity.java
@@ -1,18 +1,27 @@
 package com.niclas.notify;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.JsonWriter;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import org.json.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 
@@ -20,14 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<NoteModel> listOfNotes = new ArrayList<NoteModel>();
     private ArrayList<String> listOfTitles;
+    private SharedPreferences.Editor sharedPreferences;
     private final int EMPTYLIST = 0;
     private final int REQUESTCODE = 100;
+    private final String FILENAME = "Notify";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getData();
         fabNewNoteClicked();
         listViewItemClicked();
 
@@ -40,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
                 public void onItemClick(AdapterView parentAdapter, View view,
                                         int position, long id) {
-
                     getSpecificNoteData(listOfNotes.get(position));
                 }
             });
@@ -77,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUESTCODE)
         {
             listOfNotes.add((NoteModel) data.getExtras().getSerializable("newNote"));
+            saveToFileSystem(FILENAME);
+            Snackbar.make(findViewById(R.id.mainLayout),"Saved",2000).show();
             getData();
 
         }
@@ -85,14 +97,42 @@ public class MainActivity extends AppCompatActivity {
     private void getData(){
         if(listOfNotes.size() > EMPTYLIST){
             ListView listView = findViewById(R.id.dynamicList);
-            listView.setAdapter(getListOfTitles());
-            Snackbar.make(findViewById(R.id.mainLayout),"Saved",4000).show();
+            listView.setAdapter(getTitlesFromNoteList());
+        }else if(loadFromFileSystem(FILENAME)){
+            getData();
         }
-
     }
-    private ArrayAdapter<String> getListOfTitles(){
-        listOfTitles = new ArrayList<String>();
+    private boolean saveToFileSystem(String fileName) {
+        boolean fileSaved = true;
+        try {
+            FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(listOfNotes);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fileSaved = false;
+        }
+        return fileSaved;
+    }
 
+    private boolean loadFromFileSystem(String fileName){
+        boolean fileLoaded = true;
+        try {
+            FileInputStream fin = openFileInput(fileName);
+            ObjectInputStream oin = new ObjectInputStream(fin);
+
+            listOfNotes = (ArrayList<NoteModel>)oin.readObject();
+            oin.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+            fileLoaded = false;
+        }
+        return fileLoaded;
+    }
+
+    private ArrayAdapter<String> getTitlesFromNoteList(){
+        listOfTitles = new ArrayList<String>();
         for(int i =0 ;i < listOfNotes.size();i++) {
             listOfTitles.add(listOfNotes.get(i).getTitle());
         }
